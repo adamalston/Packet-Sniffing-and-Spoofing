9fd3a4969a84c4be3bdde28f3b86b5fdd01207a6
Adam <aalston9@gmail.com>
2020-05-25 00:12:19 -0400

    icmp

---
+++ spoof/spoof_icmp.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include "header.h"
+
+unsigned short in_cksum(unsigned short *buf, int length);
+void send_raw_ip_packet(struct ipheader *ip);
+
+// Spoof an ICMP echo request using an arbitrary source IP Address
+int main()
+{
+  char buffer[1500];
+
+  memset(buffer, 0, 1500);
+
+  // Step 1: Fill in the ICMP header.
+  struct icmpheader *icmp = (struct icmpheader *)(buffer + sizeof(struct ipheader));
+  icmp->icmp_type = 8; //ICMP Type: 8 is request, 0 is reply.
+
+  // Calculate the checksum for integrity
+  icmp->icmp_chksum = 0;
+  icmp->icmp_chksum = in_cksum((unsigned short *)icmp,
+                               sizeof(struct icmpheader));
+
+  // Step 2: Fill in the IP header.
+  struct ipheader *ip = (struct ipheader *)buffer;
+  ip->iph_ver = 4;
+  ip->iph_ihl = 5;
+  ip->iph_ttl = 20;
+  ip->iph_sourceip.s_addr = inet_addr("1.2.3.4");
+  ip->iph_destip.s_addr = inet_addr("10.0.2.7");
+  ip->iph_protocol = IPPROTO_ICMP;
+  ip->iph_len = htons(sizeof(struct ipheader) +
+                      sizeof(struct icmpheader));
+
+  // Step 3: Finally, send the spoofed packet
+  send_raw_ip_packet(ip);
+
+  return 0;
+}
