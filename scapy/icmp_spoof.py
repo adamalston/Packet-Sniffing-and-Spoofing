125660522175a19e6935a25f68085f412dae03d5
Adam <aalston9@gmail.com>
2020-06-29 04:24:23 -0400

    refactor ICMP spoof

--- scapy/icmp_spoof.py
+++ scapy/icmp_spoof.py
@@ -1,8 +1,10 @@
 from scapy.all import *
 
-print("SENDING SPOOFED ICMP PACKET.........")
-ip = IP(src="1.2.3.4", dst="93.184.216.34")
-icmp = ICMP()
-pkt = ip/icmp
+print("SENDING SPOOFED ICMP PACKET")
+
+ip = IP(src="1.2.3.4", dst="93.184.216.34") # IP Layer
+icmp = ICMP()                               # ICMP Layer
+pkt = ip/icmp                               # Construct the complete packet
 pkt.show()
+
 send(pkt, verbose=0)
