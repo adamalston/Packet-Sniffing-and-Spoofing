0261665e1681f75a486f4450e1eb58e48dfbcae8
Adam <aalston9@gmail.com>
2020-07-27 00:51:26 -0400

    refactor sniff

--- sniff/sniff.c
+++ sniff/sniff.c
@@ -7,28 +7,27 @@ struct ethheader
 {
     u_char ether_dhost[6]; // destination host address
     u_char ether_shost[6]; // source host address
-    u_short ether_type;	   // protocol type (IP, ARP, RARP, etc)
+    u_short ether_type;    // protocol type (IP, ARP, RARP, etc)
 };
 
 // IP Header
 struct ipheader
 {
-    unsigned char iph_ihl : 4,		 // IP header length
-        iph_ver : 4;				 // IP version
-    unsigned char iph_tos;			 // Type of service
-    unsigned short int iph_len;		 // IP Packet length (data + header)
-    unsigned short int iph_ident;	 // Identification
+    unsigned char iph_ihl : 4,       // IP header length
+        iph_ver : 4;                 // IP version
+    unsigned char iph_tos;           // Type of service
+    unsigned short int iph_len;      // IP Packet length (data + header)
+    unsigned short int iph_ident;    // Identification
     unsigned short int iph_flag : 3, // Fragmentation flags
-        iph_offset : 13;			 // Flags offset
-    unsigned char iph_ttl;			 // Time to Live
-    unsigned char iph_protocol;		 // Protocol type
-    unsigned short int iph_chksum;	 // IP datagram checksum
-    struct in_addr iph_sourceip;	 // Source IP address
-    struct in_addr iph_destip;		 // Destination IP address
+        iph_offset : 13;             // Flags offset
+    unsigned char iph_ttl;           // Time to Live
+    unsigned char iph_protocol;      // Protocol type
+    unsigned short int iph_chksum;   // IP datagram checksum
+    struct in_addr iph_sourceip;     // Source IP address
+    struct in_addr iph_destip;       // Destination IP address
 };
 
-void got_packet(u_char *args, const struct pcap_pkthdr *header,
-                const u_char *packet)
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 {
     struct ethheader *eth = (struct ethheader *)packet;
 
